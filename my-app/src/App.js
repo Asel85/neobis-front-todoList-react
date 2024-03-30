edfc3949a0acba68f250b48934f6a854b8df25bd
Asel <aabdykalykova186@gmail.com>
2024-03-30 22:47:12 +0100

    add delete

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,31 @@
+import { useState } from "react";
 import Todo from "./components/Todo";
+import TodoList from "./components/TodoList";
+
 
 function App() {
+
+  const [todo, setTodo] = useState([
+    {
+    id:1,
+    title:"first title",
+    status:true
+  },
+  {
+    id:2,
+    title:"first title",
+    status:true
+  },
+  {
+    id:3,
+    title:"first title",
+    status:true
+  },
+])
   return (
     <div>
       <Todo />
+      <TodoList todo={todo} setTodo={setTodo} />
     </div>
   );
 }
--- my-app/src/components/TodoList.jsx
+++ my-app/src/components/TodoList.jsx
@@ -1,11 +1,35 @@
 import React from 'react'
 
-const TodoList = () => {
+const TodoList = ({todo, setTodo}) => {
+
+function deleteTodo(id){
+  const newTodo = todo.filter((item)=>{
+    return (
+      item.id !== id
+    )
+  });
+  setTodo(newTodo);
+}
   return (
-    <div>
-      
-    </div>
-  )
+     <div className="todo-list">
+       <h3 className="todo-list__title">todo list</h3>
+      {
+        todo.map((item)=>{
+          return(
+            <div className="items" key={item.id}>
+            <div className="items__content">
+              <div className="items__text">{item.title}</div>
+              </div>
+              <div className="items__buttons">
+                <button className="items__button-edit">Edit</button>
+                <button onClick={()=>deleteTodo(item.id)} className="items__button-delete">Delete</button>
+              </div>
+          </div>
+          )
+        })
+      }
+      </div>
+   )
 }
 
 export default TodoList
